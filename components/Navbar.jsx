7fb621a1863428d68004e5e5a7f1c1b91284929c
Mengyuan Li <mli114@volvocars.com>
2023-11-01 14:28:42 +0100

    added styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,11 @@
+import logo from "../src/assets/react-icon-small.png";
+
 export default function Navbar() {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand" href="#">
-				Navbar goes here
-			</a>
+		<nav className="navbar">
+			<img className="react-logo" src={logo} alt="React Logo" />
+			<h1 className="navbar-title">ReactFacts</h1>
+			<h1 className="navbar-content">React Course - Project 1</h1>
 		</nav>
 	);
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<h1>React App</h1>
 			<Navbar />
 			<Main />
 		</>
