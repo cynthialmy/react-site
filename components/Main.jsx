35cfeb70db7ad4255890fb7e3797eabb2dd18c5a
Mengyuan Li <mli114@volvocars.com>
2023-11-01 12:32:13 +0100

    added componenets

---
+++ components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<h1>Main goes here</h1>
+				</div>
+			</div>
+		</div>
+	);
+}
